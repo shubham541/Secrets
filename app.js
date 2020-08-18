baee46ae05b2904edd365b435e58903cc13dc066
shubham541 <kumar.shubham541@gmail.com>
2020-08-18 15:33:04 +0530

    Level 3: Hashing- Used md5 for hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 
@@ -22,8 +22,6 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
-
 const User=new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
@@ -38,7 +36,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser=new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -51,7 +49,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   username=req.body.username;
-  password=req.body.password;
+  password=md5(req.body.password);
 
   User.findOne({email:username},function(err,foundUser){
     if(err){
